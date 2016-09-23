154895d47dc32b7de915b5adac8ece784578a294
Bhushan Deshmukh <bhushan.deshmukh2012@gmail.com>
2016-09-22 21:22:19 -0700

    Planet classes and interface
    committing AlienPlanet, HomePlanet and planet interface

---
+++ CodeSpanners/AlienPlanet.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class AlienPlanet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AlienPlanet extends Actor implements Planet
+{
+    boolean isBlue;
+    int weight[];
+    int planetNumber;
+    
+    /**
+     * Act - do whatever the AlienPlanet wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    
+    public void capture()
+    {
+    //to do
+    }
+    
+    public void uncapture()
+    {
+        
+    }
+}
---
+++ CodeSpanners/HomePlanet.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HomePlanet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HomePlanet extends Actor implements Planet
+{
+   int noOfSoldiers;
+   int weight[];
+    /**
+     * Act - do whatever the HomePlanet wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    /**
+     * Constructor for objects of class HomePlanet
+     */
+    public HomePlanet()
+    {
+    }
+    int getSoldierCount()
+    {
+        // put your code here
+        int value = 0;
+        return value;
+    }
+}
