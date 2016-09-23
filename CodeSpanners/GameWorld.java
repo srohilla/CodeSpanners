993ec6f0eeaa5ff50d49ef073b44ef6b7d388097
Seema Rohilla <seemarohilla16@gmail.com>
2016-09-22 20:49:35 -0700

    GameWorld
    GameWorld created

--- CodeSpanners/GameWorld.java
+++ CodeSpanners/GameWorld.java
@@ -6,16 +6,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
-{
+public class GameWorld extends World
+{   int time;
+    boolean allCaptured;
+    int noOfPlanetsCaptured;
+    int surplusSoldiers;
+    int[] planets;
+    int[] soldiers;
 
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public GameWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
     }
+    void submitScores(){
+    //to do
+    }
+    void growSoldiers(){
+    //to do
+    }
+    void check(){
+    //to do
+    }    
+    void mouseDragSoldiers(){
+    //to do
+    }
+    
 }
