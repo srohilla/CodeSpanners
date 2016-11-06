905d6073f6f7401b3f75d4412a1bc66208fc4b30
Bhushan Deshmukh <bhushan.deshmukh2012@gmail.com>
2016-11-06 15:41:55 -0800

    Adding Design Pattern to Destroyable Objects
    Adding Design Pattern to Destroyable Objects

---
+++ CodeSpanners/DestroyedState.java
@@ -0,0 +1,19 @@
+/**
+ * Write a description of class DestroyedState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DestroyedState implements DestroyableObjectsState
+{
+  
+
+    /**
+     * Constructor for objects of class CapturedState
+     */
+    public DestroyedState()
+    {
+    }
+
+
+}
---
+++ CodeSpanners/ExistingState.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class ExistingState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ExistingState implements DestroyableObjectsState
+{
+    
+    public ExistingState()
+    {
+    }
+}
