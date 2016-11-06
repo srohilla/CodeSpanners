49085a2886b52a6a19b29d09d8899e578999811d
Bhushan Deshmukh <bhushan.deshmukh2012@gmail.com>
2016-11-06 15:52:29 -0800

    Update in Design Pattern for Destroyable Objects for existing state
    Update in Design Pattern for Destroyable Objects for existing state

--- CodeSpanners/ExistingState.java
+++ CodeSpanners/ExistingState.java
@@ -4,6 +4,8 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+ /*This class will maintain the state of objects which would be act as a hinderance for the soldiers*/
 public class ExistingState implements DestroyableObjectsState
 {
     
