e97f3f00aaa4f4bc78d70d39d0c7c1c20f82bfdc
virajnilakh <virajnilakh220@gmail.com>
2016-11-13 14:16:33 -0800

    close #86
    Help button and receiver do action needed..Plz implement it

---
+++ CodeSpanners/HelpCommand.java
@@ -0,0 +1,13 @@
+public class HelpCommand implements Command {
+private Receiver r;
+private HelpButton hb;
+private HelpCommand hc;
+public void setReceiver(Receiver r1){
+    this.r = r1;
+}
+public void execute() {
+    this.r.doAction();
+
+    
+}
+}
