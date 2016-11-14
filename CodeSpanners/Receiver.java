51e7821dd766b690cae144758013705b60afb6a3
Bhushan Deshmukh <bhushan.deshmukh2012@gmail.com>
2016-11-13 16:09:08 -0800

    Committing the receiver interface
    Committing the receiver interface

--- CodeSpanners/Receiver.java
+++ CodeSpanners/Receiver.java
@@ -4,27 +4,8 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Receiver  
+public interface Receiver  
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Receiver
-     */
-    public Receiver()
-    {
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+  public void doAction();
+  
 }
